4af6fec6c1cc2f6bc414ea4e60174e3534836521
Lucas Martini <kpolucas88@gmail.com>
2020-11-05 21:25:52 -0300

    scrapper inicial horrible

---
+++ ScrapDirt.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrapDirt : MonoBehaviour
+{
+
+    public float timeToSpawn = 2f;
+    public GameObject ammoCrate;
+    float timer;
+
+    private void Start()
+    {
+        timer = timeToSpawn;
+    }
+
+    void Update()
+    {
+        SpawnBullets();
+    }
+
+    void SpawnBullets()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            timer = timeToSpawn;
+            Vector3 closeRandom = Random.insideUnitCircle;
+            Instantiate(ammoCrate, (transform.position + closeRandom), Quaternion.identity);
+        }
+    }
+}
